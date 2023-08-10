c6396c0dd0293a5f21a61a21367a7a684654cfd9
Mj <martinjordan21@hotmail.com>
2023-08-10 19:53:53 +0100

    Change the default collection method to statsd from influxDB.

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
-#k6 run --vus 10000 --duration 30m ./echo-server.js
-
-
 docker build --tag k6 k6
 
 if [ $? != 0 ]
@@ -9,4 +6,4 @@ then
 	exit -1
 fi
 	
-docker run -it --name k6 --rm --network host -e K6_VIRTUAL_USER_COUNT=10000 k6
+docker run -it --name k6 --rm --network host -e K6_VIRTUAL_USER_COUNT=$1 -e K6_OUTPUT_INTEGRATION=statsd -e K6_TEST_DURATION='10m' k6
